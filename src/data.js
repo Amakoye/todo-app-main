1ee1c8de0f9e7a6f597ab7de8e9707a6367bc9a4
Amakoye <charlesamakoye@gmail.com>
2021-08-06 21:18:51 +0300

    added placeholder data

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const todos = [
+  { title: "Complete online Javascript Course", isComplete: true },
+  { title: "Jog around the park 3x", isComplete: false },
+  { title: "10 minutes meditation", isComplete: false },
+  { title: "Read for 1 hour", isComplete: false },
+  { title: "Pick up groceries", isComplete: false },
+  { title: "Complete Todo App on Frontend Mentor", isComplete: false },
+];
