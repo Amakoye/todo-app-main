e84a04ba6d72b6985dcca987826a5285da5f19df
Amakoye <charlesamakoye@gmail.com>
2021-08-09 12:40:53 +0300

    todosReducer basic setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useReducer } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import { stateData } from "./data";
+import { todosReducer } from "./reducer/todosReducer";
 
 const TodosContext = React.createContext();
 
 const App = () => {
+  const defaultState = {
+    todos: [],
+    toggleLight: false,
+  };
+  const [state, dispatch] = useReducer(todosReducer, defaultState);
   const [todos, setTodos] = useState(stateData);
   return (
     <TodosContext.Provider>
