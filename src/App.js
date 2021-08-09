85e874eb3ba78a9d6d24297681fe9c0c8e80b59c
Amakoye <charlesamakoye@gmail.com>
2021-08-09 12:35:31 +0300

    useContext setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import { stateData } from "./data";
 
+const TodosContext = React.createContext();
+
 const App = () => {
   const [todos, setTodos] = useState(stateData);
   return (
-    <div className="App container">
-      <Header />
-      <NewTodo />
-      <Todos todos={todos} />
-    </div>
+    <TodosContext.Provider>
+      <div className="App container">
+        <Header />
+        <NewTodo />
+        <Todos todos={todos} />
+      </div>
+    </TodosContext.Provider>
   );
 };
 
